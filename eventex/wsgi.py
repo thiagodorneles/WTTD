fcd54d5dda57e7485582c543a9a0592444ddf4c7
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-09-10 00:53:39 -0300

    wgsi heroku

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -26,7 +26,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-# application = get_wsgi_application()
 application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
