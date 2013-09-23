7a1d4d90aeb741635c6bfb143eb03c05769c89af
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-09-22 22:53:53 -0300

    Widget and Field

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -24,11 +24,25 @@ class SubscriptionFormTest(TestCase):
     def test_email_is_optional(self):
         'Email is optional'
         form = self.make_validated_form(email='')
+        print form.errors
         self.assertFalse(form.errors)
 
+    def test_must_inform_email_or_phone(self):
+        'Email and Phone are optional, but one must be informed.'
+        form = self.make_validated_form(email='', phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
+
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized'
+        form = self.make_validated_form(name='THIAGO dorneles')
+        self.assertEqual('Thiago Dorneles', form.cleaned_data['name'])
+
     def make_validated_form(self, **kwargs):
-        data = dict(name='Thiago Dorneles', cpf='12345678901',
-                    email='thiagodornelesrs@gmail.com', phone='51 9246.5987')
+        data = dict(name='Thiago Dorneles', 
+                    cpf='12345678901',
+                    email='thiagodornelesrs@gmail.com', 
+                    phone_0='51',
+                    phone_1='92465987')
         data.update(kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -19,8 +19,8 @@ class SubscribeTest(TestCase):
     def test_html(self):
         'Html must contain input controls'
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 6)
-        self.assertContains(self.resp, 'type="text"', 4)
+        self.assertContains(self.resp, '<input', 7)
+        self.assertContains(self.resp, 'type="text"', 5)
         self.assertContains(self.resp, 'type="submit"')
 
     def test_csrf(self):
@@ -70,3 +70,10 @@ class SubscribeInvalidPostTest(TestCase):
     def test_dont_save(self):
         'Do not save data'
         self.assertFalse(Subscription.objects.exists())
+
+class TemplateregressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        'Check if non_field_errors are shown in template.'
+        invalid_data = dict(name='Thiago Dorneles', cpf='12345678901')
+        response = self.client.post(r('subscriptions:subscribe'), invalid_data)
+        self.assertContains(response, 'ul class="errorlist"')
