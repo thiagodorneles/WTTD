3e6f51bf312b82c26530bf6479d1639ade30d9b2
Thiago Dorneles <thiagodornelesrs@gmail.com>
2013-11-17 17:24:01 -0200

    Course

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -55,3 +55,9 @@ class Talk(models.Model):
 
     def get_absolute_url(self):
         return '/palestras/%d/' % self.pk
+
+class Course(Talk):
+    slots = models.IntegerField()
+    notes = models.TextField()
+
+    objects = PeriodManager()
